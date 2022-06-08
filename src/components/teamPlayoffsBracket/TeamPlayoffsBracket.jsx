e4c8e927358f45b46bf8e59da8f84d9c234e4aed
Kaiyang Zheng <kaiyangzheng@Kaiyangs-MacBook-Pro.local>
2022-06-07 18:19:02 -0700

    Playoffs Bracket

--- src/components/teamPlayoffsBracket/TeamPlayoffsBracket.jsx
+++ src/components/teamPlayoffsBracket/TeamPlayoffsBracket.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from 'react'
 import './teamplayoffsbracket.css'
 import _ from 'lodash'
@@ -508,120 +507,11 @@ export default function TeamPlayoffsBracket({playoffsData, teamInfo, objStanding
                         </li>
                         </>
                     })}
-=======
-import React from 'react'
-import './teamplayoffsbracket.css'
-
-export default function TeamPlayoffsBracket() {
-    return (
-        <div className="teamPlayoffsBracket">
-            <main id="tournament">
-                <ul class="round round-1">
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Lousville <span>79</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">NC A&T <span>48</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Colo St <span>84</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Missouri <span>72</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top ">Oklahoma St <span>55</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Oregon <span>68</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Saint Louis <span>64</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">New Mexico St <span>44</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Memphis <span>54</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">St Mary's <span>52</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Mich St <span>65</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Valparaiso <span>54</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Creighton <span>67</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Cincinnati <span>63</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Duke <span>73</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Albany <span>61</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-                </ul>
-                <ul class="round round-2">
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Lousville <span>82</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Colo St <span>56</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Oregon <span>74</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Saint Louis <span>57</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top ">Memphis <span>48</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Mich St <span>70</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top ">Creighton <span>50</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Duke <span>66</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-                </ul>
-                <ul class="round round-3">
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Lousville <span>77</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Oregon <span>69</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top ">Mich St <span>61</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom winner">Duke <span>71</span></li>
-
-                    <li class="spacer">&nbsp;</li>
-                </ul>
-                <ul class="round round-4">
-                    <li class="spacer">&nbsp;</li>
-
-                    <li class="game game-top winner">Lousville <span>85</span></li>
-                    <li class="game game-spacer">&nbsp;</li>
-                    <li class="game game-bottom ">Duke <span>63</span></li>
->>>>>>> 73d2d6fa8376ef8b0db9ca68c688a3e81e999aa6
 
                     <li class="spacer">&nbsp;</li>
                 </ul>
                 <ul className="round round-5">
                     <li className="space">&nbsp;</li>
-<<<<<<< HEAD
                     <li className="game game-top">
                         {champion && 
                             <div className="teamPlayoffsContainer">
@@ -630,15 +520,8 @@ export default function TeamPlayoffsBracket() {
                             </div>
                         }
                     </li>
-=======
-                    <li className="game game-top"></li>
->>>>>>> 73d2d6fa8376ef8b0db9ca68c688a3e81e999aa6
                 </ul>
             </main>
         </div>
     )
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 73d2d6fa8376ef8b0db9ca68c688a3e81e999aa6
